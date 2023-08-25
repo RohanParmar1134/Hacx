14617aa0f037542e40d21030c0f8b2993802e3bc
Rohan <rp331563@gmail.com>
2023-08-26 02:29:03 +0530

    init

---
+++ src/components/login.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import axios from "axios";
+// import "./Database.css";
+import Spline from "@splinetool/react-spline";
+// const canvas = document.getElementById('canvas3d');
+// const app = new Application(canvas);
+// app.load('https://prod.spline.design/hhL6pMJZDKnM7rjh/scene.splinecode');
+
+function Database() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await axios.post(
+      "http://localhost:8000/register",
+      JSON.stringify({ name, email }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    console.log(result);
+    try {
+      if (result.data.alert == 1) {
+        if (result.data.email != "") {
+          alert("Email already exists");
+        } else {
+          alert("User already register");
+        }
+      } else {
+        alert("Data saved succesfully");
+        setEmail("");
+        setName("");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      {/* <iframe src='https://my.spline.design/noisedisplacecopy-effbdbd06e5871ecd6653f365b62c17d/' frameborder='0' width='100%' height='100%'></iframe> */}
+      <div>
+        <h1 className="text-center">Database</h1>
+        <br />
+        <form action="">
+          <input
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="submit" onClick={handleOnSubmit}>
+            submit
+          </button>
+          <div>
+            {/* <Spline scene="https://prod.spline.design/hhL6pMJZDKnM7rjh/scene.splinecode" /> */}
+            <Spline scene="https://prod.spline.design/zJ1Yav9dn06NEslU/scene.splinecode" />
+            bjbkjbkjfjbkj
+            <input style={{zIndex:1}} type="date" />
+          </div>
+          {/* <button type="submit" onClick={delete_data}>submit</button> */}
+        </form>
+      </div>
+    </>
+  );
+}
+export default Database;
---
+++ src/database/connect.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { json } = require("body-parser");
+mongoose
+  .connect("mongodb+srv://rp331563:Rohan1134@ljinsider.ril4fay.mongodb.net/")
+  .then(() => console.log("connection successed"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  id:{
+      type:Date,
+      default: new Date().toLocaleString()
+  }
+});
+
+const User = mongoose.model("users", UserSchema);
+console.log("App running at 8000");
+app.use(express.json());
+app.use(cors());
+
+app.post("/", (req, res) => {
+  res.send("App is working");
+});
+
+app.post("/register", async (req, res, next) => {
+  const user = new User(req.body);
+  var count = await User.findOne({email: user.email});
+  console.log(count);
+  if (count != null) {
+    if (count.email === user.email){
+      res.send({ alert: 1 , email : count.email});
+    }else{
+      res.send({alert:1})
+    }
+  } else {
+    console.log("next");
+    next();
+  }
+});
+
+app.post("/register", async(req,res) => {
+   try{
+    const user = new User(req.body);
+    result = user.toObject();
+    console.log(result);
+    await user.save()
+    res.send(req.body);
+  } catch (e) {
+    res.send("Something Went Wrong");
+  }
+});
+app.listen(8000);
+
+
